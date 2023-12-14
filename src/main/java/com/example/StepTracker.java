e619697a1a054b824f5825e7f593595161d5f1d6
Michael0424 <0424michael@gmail.com>
2023-12-14 11:22:16 +0800

    done

--- src/main/java/com/example/StepTracker.java
+++ src/main/java/com/example/StepTracker.java
@@ -2,37 +2,56 @@ package com.example;
 
 import java.lang.reflect.Constructor;
 
-
-public class StepTracker{
+public class StepTracker {
     private int minSteps;
     private int totalSteps;
     private int totalDays;
     private int activeDays;
 
-    public StepTracker(int minSteps){
-    
-        this.minSteps=minSteps;
-        this.totalSteps=totalSteps;
-        this.totalDays=totalDays;
-        this.activeDays=activeDays;
-    }
+    public StepTracker(int minSteps) {
 
+        this.minSteps = minSteps;
+        this.totalSteps = 0;
+        this.totalDays = 0;
+        this.activeDays = 0;
+    }
 
     public int getMinSteps() {
         return minSteps;
     }
-    public int gettotalSteps(){
+
+    public int gettotalSteps() {
         return totalSteps;
     }
-    public int gettotalDays(){
+
+    public int gettotalDays() {
         return totalDays;
     }
-    public int getactiveDays(){
+
+    public int getactiveDays() {
         return activeDays;
     }
 
+    public void addDailySteps(int steps) {
+        totalSteps += steps;
+        if (steps >= minSteps) {
+            activeDays += 1;
+        }
+        totalDays += 1;
+
+    }
+    
+
+    public double averageSteps() {
+        return totalDays = totalSteps / totalDays;
+    }
+
+    public void dailySteps(){
+        
+    }
+    
 
 
 }
 
-//write your class implementation here
+// write your class implementation here
